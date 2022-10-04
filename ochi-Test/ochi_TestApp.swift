ae921e7d571a9ab70fe7807a9636869d5a335544
I Made Ocy Darma Putra <ocy.darma@indochat.co.id>
2022-10-04 10:49:39 +0700

    Initial Commit

---
+++ ochi-Test/ochi_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ochi_TestApp.swift
+//  ochi-Test
+//
+//  Created by I Made Ocy Darma Putra on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ochi_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ochi-TestUITests/ochi_TestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ochi_TestUITestsLaunchTests.swift
+//  ochi-TestUITests
+//
+//  Created by I Made Ocy Darma Putra on 04/10/22.
+//
+
+import XCTest
+
+final class ochi_TestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
